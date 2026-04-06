106b01603be78202ffd65c37fd82172f85cfe6f6
YuransiS <yura3zaxar@outlook.com>
2026-04-06 19:50:55 +0300

    "RemoveStatementBlockAndAddExpertInstagramLink"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import { Block1Hero } from "@/components/blocks/Block1Hero";
 import { Block2Learning } from "@/components/blocks/Block2Learning";
 import { Block3Audience } from "@/components/blocks/Block3Audience";
-import { Block4Statement } from "@/components/blocks/Block4Statement";
 import { Block5Expert } from "@/components/blocks/Block5Expert";
 import { Block6Registration } from "@/components/blocks/Block6Registration";
 import { Block7FAQ } from "@/components/blocks/Block7FAQ";
@@ -14,7 +13,6 @@ export default function Home() {
       <Block1Hero />
       <Block2Learning />
       <Block3Audience />
-      <Block4Statement />
       <Block5Expert />
       <Block6Registration />
       <Block7FAQ />
